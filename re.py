a9b77a6d18e3736baac1b79e00de232a465c0781
jsonsnow <1183010734@qq.com>
2018-09-05 18:32:21 +0800

    add

--- re.py
+++ re.py
@@ -95,4 +95,102 @@ class Query(object):
 
 with Query('Bob') as q:
 	q.query()
+
+
+from contextlib import contextmanager
+class Query(object):
+	def __init__(self, name):
+		self.name = name
+	def query(self):
+		print('Query info about %s ...' % self.name)
+
+
+@contextmanager
+def create_query(name):
+	print('Begin')
+	q = Query(name)
+	yield q
+	print('End')
+
+with create_query('Bob') as q:
+	q.query()
 	
+
+@contextmanager
+def tag(name):
+	print('<%s>' % name)
+	yield
+	print('</%s>' % name)
+
+with tag('h1'):
+	print('hello')
+	print('world')
+
+
+from contextlib import closing
+from urllib.request import urlopen
+
+with closing(urlopen('https://www.python.org')) as page:
+	for line in page:
+		print(line)
+
+
+@contextmanager
+def closing(thing):
+	try:
+		yield thing
+	finally:
+		thing.close()
+
+
+from urllib import request
+import json
+
+
+def fetch_data(url):
+	req = request.Request(url)
+	with request.urlopen(req) as f:
+		print('Status:', f.status, f.reason)
+		for k,v in f.getheaders():
+			print('%s:%s' % (k, v))
+		data = f.read()
+		data = json.loads(data)
+		return data
+
+
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
+
+
+from xml.parsers.expat import ParserCreate
+
+class DefaultSaxHandler(object):
+
+	def start_element(self, name, attrs):
+		print('sax:start_element: %s, attrs %s' % (name, str(attrs)))
+
+	def end_element(self, name):
+		print('sax:end_element: %s ' % name)
+
+	def char_data(self, text):
+		print('sax:char_data:%s' % text)
+
+xml = r'''<?xml version="1.0"?>
+<ol>
+    <li><a href="/python">Python</a></li>
+    <li><a href="/ruby">Ruby</a></li>
+</ol>
+'''
+
+handler = DefaultSaxHandler()
+parsers = ParserCreate()
+parsers.StartElementHandler = handler.start_element
+parsers.EndElementHandler = handler.end_element
+parsers.CharacterDataHandler = handler.char_data
+parsers.Parse(xml)
+
+
+
