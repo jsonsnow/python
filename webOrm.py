e9ff1689aa007689b6250459daf9646cf462170f
jsonsnow <1183010734@qq.com>
2018-09-13 19:22:16 +0800

    add

--- webOrm.py
+++ webOrm.py
@@ -3,6 +3,7 @@
 
 import asyncio
 import logging;logging.basicConfig(level=logging.INFO)
+import aiomysql
 
 # async def hello():
 # 	print('hello world!')
@@ -33,4 +34,43 @@ import logging;logging.basicConfig(level=logging.INFO)
 # loop.run_forever()
 
 async def create_pool(loop, **kw):
-	lo
+	logging.info('create database connection pool...')
+	global __pool
+	__pool = await aiomysql.create_pool(
+		host=kw.get('host','localhost'),
+		port=kw.get('port',3306),
+		user=kw['user'],
+		password=kw['password'],
+		db=kw['db'],
+		charset=kw.get('charset','utf8'),
+		autocommit=kw.get('autocommit',True),
+		maxsize=kw.get('maxsize',10),
+		minsize=kw.get('minsize',1),
+		loop=loop)
+
+async def select(sql, args, size=None):
+	log(sql,args)
+	global __pool
+	with yield from __pool as conn:
+		cur = await conn.cursor(aiomysql.DictCursor)
+		await from cur.execute(sql.replace('?','%s'), args, or ())
+		if size:
+			rs = await cur.fetchmany(size)
+		else:
+			rs = await cur.fetchall()
+		await cur.close()
+		logging.info('rows return:%s' % len(rs))
+		return rs
+
+async def execute(sql, args):
+	log(sql)
+	with await __pool as conn:
+		try:
+			cur = await conn.cursor()
+			await cur.execute(sql.replace('?','%s'),args)
+			affected = cur.rowcount()
+			await cur.close()
+		except BaseException as e:
+			raise 
+		return affected
+
